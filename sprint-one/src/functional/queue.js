9ff87122bfb7aab6b826fac328c0c4693cf1ad1a
shuichen17 <shuichen17@gmail.com>
2019-04-10 13:19:51 -0700

    finish queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,40 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (_.isEmpty(storage)){
+      
+       storage[0] = value;
+     
+    } else {
+       var arr = Object.keys(storage);
+       var maxIndex = Math.max(...arr);
+       storage[maxIndex + 1] = value;
+
+    }
+    return storage;
+
   };
 
   someInstance.dequeue = function() {
+    if(_.isEmpty(storage)){
+        return null;
+    } else {
+        var arr = Object.keys(storage);
+        var minIndex = Math.min(...arr);
+        var del = storage[minIndex];
+        delete storage[minIndex];
+        return del;
+    }
+    
   };
 
   someInstance.size = function() {
+
+  if(_.isEmpty(storage)){
+       return 0;
+    } else {
+      return Object.keys(storage).length;
+    }
   };
 
   return someInstance;
