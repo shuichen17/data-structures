6aa3fc7568f033dc8018b933ed215be3ccf8fe76
shuichen17 <shuichen17@gmail.com>
2019-04-11 19:53:56 -0700

    finish tree

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,29 @@
 
 // Instantiate a new graph
 var Graph = function() {
+ this.graph = {};
+
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+    this.graph[node] = [];
+
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+    for (var key in this.graph) {
+      if (Number(key) === node) {
+        return true;
+      }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+      delete this.graph[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
