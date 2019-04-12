45e9ff4bf402ff52bf5516330b01c1b92cc91c61
shuichen17 <shuichen17@gmail.com>
2019-04-12 11:09:19 -0700

    Completed graph!

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -29,18 +29,40 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+   if(this.contains(fromNode) && this.contains(toNode)){
+    if (this.graph[fromNode].includes(toNode) || this.graph[toNode].includes(fromNode)) {
+    return true;
+  }
+}
+  
+  return false;
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if(this.contains(fromNode)){
+    this.graph[fromNode].push(toNode);
+}
+  
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+    var index = this.graph[fromNode].indexOf(toNode);
+    this.graph[fromNode].splice(index, 1);
+  
+
 };
+
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  var keys = Object.keys(this.graph);
+   for (var i = 0; i < keys.length; i++){
+       cb(Number(keys[i]));
+   }
+  
 };
 
 /*
