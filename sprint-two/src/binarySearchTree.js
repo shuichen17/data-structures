16dc12e3d3d7fda8ca88382ba25a457d48a0fc96
shuichen17 <shuichen17@gmail.com>
2019-04-13 00:04:30 -0700

    finish binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,32 +13,70 @@ var treeMethod = {};
 treeMethod.insert = function (value) {
   // check value we want to insert to the current node
   // if the value is less than current node, then add it left of current node
-  
-  if (value < this.value) {
-     if (this.left !== undefined){
-     this.left.insert(value);
-    this.left = BinarySearchTree(value);
-    } else {
-     this.left = BinarySearchTree(value);
-      }
-    
+  if(this.value === undefined){
+      this.value = value;
+      return;
+
   } else {
+    if (value < this.value) {
+     if(this.left !== null) {
+      this.left.insert(value);
+     } else {
+      this.left = BinarySearchTree(value);
+     }
+     
+    //this.left = BinarySearchTree(value);
+    } 
+    
+
   // if value is greater than current node, add it to right of current node
-      if (this.right !== undefined){
-       this.right.insert(value);
-      this.right = BinarySearchTree(value);
-      } else {
-       this.right = BinarySearchTree(value);
+      else {
+        if(this.right !== null){
+         this.right.insert(value); 
+        } else {
+          this.right = BinarySearchTree(value);
+        }
+       
+  
         }
   }
-   
+  
 }
 
 treeMethod.contains = function(value) {
-
+  if (this.value !== undefined){
+    if(this.value === value){
+    return true;
+   } else {
+    if(this.left !== null){
+     if(this.left.contains(value)){
+      return true;
+     }; 
+    }
+    if(this.right !== null){
+    if(this.right.contains(value)){
+      return true;
+    };  
+    }
+   }
+  }
+   
+   return false;
 };
 
 treeMethod.depthFirstLog = function(cb) {
+  if(this.value === undefined){
+    return;
+  } else {
+    cb(this.value);
+    if(this.left !== null){
+      this.left.depthFirstLog(cb);
+    }
+    if(this.right !== null){
+      this.right.depthFirstLog(cb);
+    }
+
+  }
 };
 /*
  * Complexity: What is the time complexity of the above functions?
