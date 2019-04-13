fec6c8bf26fee576e06ae20639d2dc2c1ff4fe32
shuichen17 <shuichen17@gmail.com>
2019-04-13 10:24:22 -0700

    Completed binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -11,34 +11,67 @@ _.extend(tree, treeMethod);
 
 var treeMethod = {};
 treeMethod.insert = function (value) {
-  // check value we want to insert to the current node
-  // if the value is less than current node, then add it left of current node
-  
+  // first we check if the tree is empty
+  // if the tree is empty, set the value as the root position
+  if (this.value === undefined){ //recursion start
+        this.value = value;
+        return;
+
+} else { //if the tree isn't empty, we scan each node to compare node to value
+  // if the value is less than current node, 
   if (value < this.value) {
-     if (this.left !== undefined){
-     this.left.insert(value);
-    this.left = BinarySearchTree(value);
-    } else {
-     this.left = BinarySearchTree(value);
-      }
-    
+    if (this.left === null){ //if current node's left is empty, set value to left position
+      this.left = BinarySearchTree(value);
+    } else { //if current node's left is not empty, we do left recursion until node's left is empty
+      this.left.insert(value);
+    } 
   } else {
   // if value is greater than current node, add it to right of current node
-      if (this.right !== undefined){
-       this.right.insert(value);
-      this.right = BinarySearchTree(value);
-      } else {
+      if (this.right === null){//if current node's right is empty, set value to right position
        this.right = BinarySearchTree(value);
-        }
+      } else {
+        this.right.insert(value); //if current node's right is not empty, we do right recursion until node's right is empty
+      }
+    }
   }
-   
-}
+};
 
 treeMethod.contains = function(value) {
+    if (this.value !== undefined){
+    if (this.value === value){
+       return true;
+    } else {
+      if (this.left !== null){
+        if(this.left.contains(value)){
+          return true;
+        }
+      }
+      if (this.right !== null) {
+        if(this.right.contains(value)){
+          return true;
+      }
+      }
+      return false;
+}
+  
 
+} 
+  
 };
 
 treeMethod.depthFirstLog = function(cb) {
+   if (this.value !== undefined){
+      cb(this.value);
+      if (this.left !== null){
+        this.left.depthFirstLog(cb); 
+      }
+      if (this.right !== null) {
+        this.right.depthFirstLog(cb);
+      }
+    }
+
+
+
 };
 /*
  * Complexity: What is the time complexity of the above functions?
